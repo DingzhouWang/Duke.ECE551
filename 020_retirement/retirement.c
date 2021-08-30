6f276ceca89130d1e31845a13b6fb9ea06e86d90
dw299 <dw299@duke.edu>
2021-08-29 22:44:05 -0400

    ECE551-20

---
+++ 020_retirement/retirement.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _retire_info {
+  int months;             //number of months it is applicable to
+  double contribution;    //contribute or spent from account per month
+  double rate_of_return;  //return rate
+} retire_info;
+
+//typedef struct _retire_info retire_info;
+
+void retirement(int startAge, double initial, retire_info working, retire_info retired) {
+  for (int i = startAge; i < startAge + working.months + retired.months; i++) {
+    printf("Age %3d month %2d you have $%.2lf\n", i / 12, i % 12, initial);
+    if (i < startAge + working.months) {
+      initial += initial * working.rate_of_return + working.contribution;
+    }
+    if (i >= startAge + working.months) {
+      initial += initial * retired.rate_of_return + retired.contribution;
+    }
+  }
+}
+
+int main() {
+  retire_info working, retired;
+  working.months = 489;
+  working.contribution = 1000;
+  working.rate_of_return = 0.045 / 12;
+  retired.months = 384;
+  retired.contribution = -4000;
+  retired.rate_of_return = 0.01 / 12;
+  retirement(327, 21345, working, retired);
+  return 0;
+}
