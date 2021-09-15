89e967a3ad14c38ffb9fe6f094faf85850250e7a
dw299 <dw299@duke.edu>
2021-09-15 00:45:08 -0400

    ECE551-37

---
+++ 037_array_subseq/maxSeq.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq(int * array, size_t n) {
+  size_t ans = 0;
+  size_t cnt = 0;
+  if (n == 0)
+    return ans;
+  for (size_t i = 1; i < n; i++) {
+    if (array[i] > array[i - 1]) {
+      cnt++;
+    }
+    else {
+      if (ans < cnt)
+        ans = cnt;
+      cnt = 1;
+    }
+  }
+  if (ans < cnt)
+    return cnt;
+  return ans;
+}
