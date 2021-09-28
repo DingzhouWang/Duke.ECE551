836c5fd1fe161cca892905d554d7a593322681d5
dw299 <dw299@duke.edu>
2021-09-28 16:42:39 -0400

    ECE551-53

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,8 +14,45 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void operations(FILE * f) {
+  char ** array_ = NULL;
+  char * string_line = NULL;
+  size_t size_ = 0;
+  size_t len = 0;
+  while (getline(&string_line, &len, f) >= 0) {
+    array_ = realloc(array_, (size_ + 1) * sizeof(*array_));
+    array_[size_] = string_line;
+    string_line = NULL;
+    size_++;
+  }
+  free(string_line);
+  sortData(array_, size_);
+
+  for (size_t i = 0; i < size_; i++) {
+    printf("%s", array_[i]);
+    free(array_[i]);
+  }
+  free(array_);
+}
+
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
-
+  if (argc != 1) {
+    for (int i = 1; i < argc; i++) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        fprintf(stderr, "Cannot open the file!\n");
+        exit(EXIT_FAILURE);
+      }
+      operations(f);
+      if (fclose(f) != 0) {
+        fprintf(stderr, "Cannot close the file!\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+  else {
+    operations(stdin);
+  }
   return EXIT_SUCCESS;
 }
