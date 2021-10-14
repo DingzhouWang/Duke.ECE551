cc6f867b514e5384d7a4435820a16aee2813742d
dw299 <dw299@duke.edu>
2021-10-14 17:39:46 -0400

    60-1

---
+++ 060_eval2/rand_story.h
@@ -0,0 +1,13 @@
+#ifndef __RAND_STORY_H__
+#define __RAND_STORY_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "provided.h"
+
+//any functions you want your main to use
+void parse_template(char * line);
+void read_template(FILE * f);
+
+#endif
