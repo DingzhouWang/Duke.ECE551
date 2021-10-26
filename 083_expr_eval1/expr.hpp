1d05dda350fa0703b68da422418641cdc817df6d
dw299 <dw299@duke.edu>
2021-10-26 14:42:29 -0400

    83

---
+++ 083_expr_eval1/expr.hpp
@@ -0,0 +1,39 @@
+#include <cstdlib>
+#include <sstream>
+#include <string>
+
+class Expression {
+ public:
+  virtual std::string toString() const = 0;
+  virtual ~Expression() {}
+};
+
+class NumExpression : public Expression {
+  long num;
+
+ public:
+  NumExpression(long n) : num(n) {}
+  virtual std::string toString() const {
+    std::stringstream ss;
+    ss << num;
+    std::string s = ss.str();
+    return s;
+  }
+  virtual ~NumExpression() {}
+};
+
+class PlusExpression : public Expression {
+  Expression * lhs;
+  Expression * rhs;
+
+ public:
+  PlusExpression(Expression * l, Expression * r) : lhs(l), rhs(r) {}
+  virtual std::string toString() const {
+    std::string s = "(" + lhs->toString() + " + " + rhs->toString() + ")";
+    return s;
+  }
+  ~PlusExpression() {
+    delete lhs;
+    delete rhs;
+  }
+};
