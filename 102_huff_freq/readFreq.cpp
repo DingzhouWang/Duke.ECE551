d47136d79aa416150c2a4cf192730d2135359681
dw299 <dw299@duke.edu>
2021-11-16 14:31:31 -0500

    102

--- 102_huff_freq/readFreq.cpp
+++ 102_huff_freq/readFreq.cpp
@@ -1,9 +1,14 @@
-#include <stdio.h>
-#include <cstdlib>
 #include "readFreq.h"
 
+#include <stdio.h>
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
 void printSym(std::ostream & s, unsigned sym) {
-  if (sym > 256 ) {
+  if (sym > 256) {
     s << "INV";
   }
   else if (sym == 256) {
@@ -21,4 +26,21 @@ void printSym(std::ostream & s, unsigned sym) {
 }
 uint64_t * readFrequencies(const char * fname) {
   //WRITE ME!
+  ifstream file;
+  file.open(fname);
+  if (!file.is_open()) {
+    cerr << "cannot open the file: " << fname << endl;
+    exit(EXIT_FAILURE);
+  }
+  uint64_t * count = new uint64_t[257]();
+  int c;
+  while ((c = file.get()) != EOF) {
+    if (c > 255) {
+      continue;
+    }
+    count[c]++;
+  }
+  count[256] = 1;
+  file.close();
+  return count;
 }
