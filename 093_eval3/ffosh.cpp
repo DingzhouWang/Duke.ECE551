0c4b0caa489afd7db3de5a6e7552e8fc05c64fcf
dw299 <dw299@duke.edu>
2021-11-17 22:17:25 -0500

    093-2

--- 093_eval3/ffosh.cpp
+++ 093_eval3/ffosh.cpp
@@ -7,50 +7,34 @@
 #include <string>
 #include <vector>
 
-void process_cmd(const std::string & line) {
-  pid_t p1 = -1;
-  //std::vector<std::string> cmd;
-  p1 = fork();
-  if (p1 < 0) {
-    std::cerr << "cannot fork!" << std::endl;
-    exit(EXIT_FAILURE);
-  }
-  else if (p1 > 0) {
-    int status;
-    //learn from linux man page
-    pid_t wpid = waitpid(p1, &status, WUNTRACED);
-    if (wpid == -1) {
-      std::cerr << "waitpid error!" << std::endl;
-      exit(EXIT_FAILURE);
-    }
-    if (WIFEXITED(status)) {
-      if (WEXITSTATUS(status) == 0)
-        std::cout << "Program was successful" << std::endl;
-      else
-        std::cout << "Program failed with code " << WEXITSTATUS(status) << std::endl;
-    }
-    if (WIFSIGNALED(status)) {
-      std::cout << "Terminated by signal " << WTERMSIG(status) << std::endl;
-    }
-  }
-  else if (p1 == 0) {
-    char * tmp_cmd = (char *)line.c_str();
-    char * cmd[] = {tmp_cmd, NULL};
-    if (execve(tmp_cmd, cmd, NULL) < 0)
-      exit(EXIT_FAILURE);
-  }
-}
+#include "my_shell.hpp"
 
-int main() {
+int main(int argc, char * argv[]) {
   std::string line;
+  Shell My_Shell;
+  size_t cnt = 1;
+  size_t num = argc - 1;
+  while (num) {
+    std::string tmp_arg = argv[cnt];
+    line = line + tmp_arg + " ";
+    num--;
+    cnt++;
+  }
+  //std::cout << line << "1" << std::endl;
+  if (line.size()) {
+    My_Shell.execute(line);
+  }
+  //std::cout << line << std::endl;
   std::cout << "ffosh$ ";
   while (getline(std::cin, line)) {
     if (line == "exit()") {
       std::cout << "break now!!!!" << std::endl;
       break;
     }
-    std::cout << line << std::endl;
-    process_cmd(line);
+    //std::cout << line << std::endl;
+    //process_cmd(line);
+    //Shell My_Shell;
+    My_Shell.execute(line);
     std::cout << "ffosh$ ";
   }
   return (EXIT_SUCCESS);
--- 093_eval3/my_shell.hpp
+++ 093_eval3/my_shell.hpp
@@ -4,21 +4,23 @@
 #include <string>
 #include <vector>
 
-class shell {
+class Shell {
  private:
   char * path;
   std::vector<char *> env_paths;
-  char ** argv;
+  char ** argv;  //not used
+  std::vector<std::string> argument_p;
 
  public:
-  shell();
-  ~shell();
+  Shell(){};
+  ~Shell(){};
   bool init_envpath();
   std::string simplifyPath(std::string path);
   void process_cmd(const std::string & line);
   void set_path(const char * paths);
-  void set_argv(const char * l);
+  void set_argv();
   void execute(const std::string & line);
   void free_argv();
   void free_path();
+  void parse_input(const std::string & input);
 };
